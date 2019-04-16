80556c49832a07c84535a87ca459d41e057efc04
Anton B <anton@absoftware.mobi>
2019-04-16 16:11:48 +0300

    Added googleforms, calendar and iframe support

--- Source/HtmlModuleRouter.swift
+++ Source/HtmlModuleRouter.swift
@@ -11,6 +11,8 @@ import AppBuilderCoreUI
 
 import WebKit
 
+import XMLMapper
+
 public enum HtmlModuleRoute: Route {
     case root
 }
@@ -22,7 +24,7 @@ public class HtmlModuleRouter: BaseRouter<HtmlModuleRoute> {
     }
     
     public override func prepareTransition(for route: HtmlModuleRoute) -> RouteTransition {
-        return RouteTransition(module: HtmlController(data: module?.data), isRoot: false, isAnimated: true, showNavigationBar: true, showTabBar: false)
+        return RouteTransition(module: HtmlController(type: module?.config?.type, data: module?.data), isRoot: false, isAnimated: true, showNavigationBar: true, showTabBar: false)
     }
     
     public override func rootTransition() -> RouteTransition {
@@ -33,12 +35,14 @@ public class HtmlModuleRouter: BaseRouter<HtmlModuleRoute> {
 class HtmlController: BaseViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
+    private var type: String?
     private var data: DataModel?
     
     private var activityIndicator: UIActivityIndicatorView?
     
-    public convenience init(data: DataModel?) {
+    public convenience init(type: String?, data: DataModel?) {
         self.init()
+        self.type = type
         self.data = data
     }
     
@@ -75,7 +79,51 @@ class HtmlController: BaseViewController, WKNavigationDelegate {
             webView.load(URLRequest(url: url))
             webView.allowsBackForwardNavigationGestures = true
         } else if let code = self.data?.code {
-            
+            switch self.type {
+            case "googleform", "calendar":
+                let processed = code.replacingOccurrences(of: "&", with: "&amp;")
+
+                let iframe = XMLMapper<IFrameModel>().map(XMLString: processed)
+                if let src = iframe?.src, let url = URL(string: src) {
+                    activityIndicator.startAnimating()
+
+                    webView.load(URLRequest(url: url))
+                }
+            default:
+                let screenWidth = UIScreen.main.bounds.width
+                var screenHeight = UIScreen.main.bounds.height
+                var padding: UIEdgeInsets = .zero
+                
+                if #available(iOS 11.0, *) {
+                    let window = UIApplication.shared.keyWindow
+                    padding.bottom = window?.safeAreaInsets.bottom ?? 0
+                    padding.top = window?.safeAreaInsets.top ?? 0
+                }
+                screenHeight -= padding.top + padding.bottom
+                
+                let processed = code.replacingOccurrences(of: "&", with: "&amp;")
+                if var iframe = XMLMapper<IFrameModel>().map(XMLString: processed) {
+                    iframe.scrolling = "no"
+                    
+                    if iframe.width != nil {
+                        iframe.width = "\(screenWidth)"
+                    } else {
+                        iframe.width = "300"
+                    }
+                    
+                    if iframe.height != nil {
+                        iframe.height = "\(screenHeight)"
+                    } else {
+                        iframe.height = "400"
+                    }
+                    
+                    let content = iframe.toXMLString() ?? ""
+                    
+                    let htmlString = "<html><meta name=\"viewport\" content=\"width=\(screenWidth)\"/><head></head><body style=\"margin:0; padding:0\"><div>\(content)</div></body></html>"
+                    
+                    webView.loadHTMLString(htmlString, baseURL: nil)
+                }
+            }
         }
     }
     
@@ -87,5 +135,34 @@ class HtmlController: BaseViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         self.activityIndicator?.stopAnimating()
+    }
+}
+
+struct IFrameModel: Codable, XMLMappable {
+    public var src: String?
+    public var width: String?
+    public var height: String?
+    public var style: String?
+    public var scrolling: String?
+    public var frameborder: String?
+    public var marginheight: String?
+    public var marginwidth: String?
+    
+    // XML Mapping
+    public var nodeName: String! = "iframe"
+    
+    public init?(map: XMLMap) {
+        self.mapping(map: map)
+    }
+    
+    public mutating func mapping(map: XMLMap) {
+        src <- map.attributes["src"]
+        width <- map.attributes["width"]
+        height <- map.attributes["height"]
+        style <- map.attributes["style"]
+        scrolling <- map.attributes["scrolling"]
+        frameborder <- map.attributes["frameborder"]
+        marginheight <- map.attributes["marginheight"]
+        marginwidth <- map.attributes["marginwidth"]
     }
 }
